e556acdfaa2724fdc3f075491ab9f71c78fa05d6
baluramk6 <baluramk6.gmail.com>
2022-12-22 13:08:41 +0530

    ChatPage

--- pages/chats.js
+++ pages/chats.js
@@ -1,5 +1,26 @@
-import React from "react";
+import { useRouter } from "next/dist/client/router";
+import React, { useContext, useState, useEffect, useReducer } from "react";
+
+import { Context } from "../context"
+import dynamic from "./next/dynamic"
+
+const ChatEngine = dynamic(() => {
+  import("react-chat-engine").thne((module) => module.ChatEngine)
+})
+
+const MessageFormSocial = dynamic(() => {
+  import("react-chat-engine").thne((module) => module.MessageFormSocial)
+})
 
 export default function Chats() {
-  return <div className="background">chats</div>;
+  const { username, secret } = useContext(Context)
+  const [showChat, setShowChat] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+
+  })
+  return <div className="background">
+
+  </div>;
 }
